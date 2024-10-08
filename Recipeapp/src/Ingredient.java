18e879d232eaec764255d463d2057ef7ed1bad6b
alaahossamelsaeed <163353424+alaahossamelsaeed@users.noreply.github.com>
2024-10-08 05:22:01 +0300

    upgrade

--- Recipeapp/src/Ingredient.java
+++ Recipeapp/src/Ingredient.java
@@ -8,26 +8,33 @@ public class Ingredient {
         this.quantity = quantity;
         this.unit = unit;
     }
+
     //getter methods
     public String getName() {
         return name;
     }
+
     public String getUnit() {
         return unit;
     }
+
     public float getquantity() {
         return quantity;
     }
+
     //setter methods
     public void setName(String name) {
         this.name =name;
     }
+
     public void setUnit(String UNIT) {
         this.unit = UNIT;
     }
+
     public void setquantity(float quantity) {
         this.quantity = quantity;
     }
+
     public String toString() {
     return quantity+" "+unit+" of "+name;
     }
